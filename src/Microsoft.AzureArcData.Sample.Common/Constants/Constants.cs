b98d8d89b4f4caf0708411dd263c0cce1077705d
unknown <mdrakiburrahman@gmail.com>
2023-07-26 06:58:36 -0700

    Change the Job Partition

--- src/Microsoft.AzureArcData.Sample.Common/Constants/Constants.cs
+++ src/Microsoft.AzureArcData.Sample.Common/Constants/Constants.cs
@@ -8,7 +8,7 @@ namespace Microsoft.AzureArcData.Sample.Common.Constants
 {
     public class JobConstants
     {
-        public const string jobPartitionName = "jobs";
+        public const string jobPartitionName = "MSSQLSERVER";
         public const string backendEnvVarName = "JOB_BACKEND";
         public const string jobTableName = "arcJobDefinitions";
     }
